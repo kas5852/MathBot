2fc1fd3a8e361026a013a2fc4817535261d994ce
riaupreti3 <56850506+riaupreti3@users.noreply.github.com>
2019-10-24 08:52:23 -0500

    Update pybot.py

--- pybot.py
+++ pybot.py
@@ -24,8 +24,19 @@ small_talk = ['hi there!',
               'not so good',
               'sorry to hear that.',
               'what\'s your name?',
-              'i\'m pybot. ask me a math question, please.']
+              'i\'m mathbot. ask me a math question, please.']
 
+farewell_talk = ['bye',
+                 'goodbye! you did so well!',
+                 'bye!',
+                 'you have improved so much!,
+                 'end',
+                 'i hope this was helpful!',
+                 'come back soon!',
+                 'hope to see you soon!',
+                 'come back for more math!',
+                 'i hope you enjoyed your experience!']
+              
 math_talk_1 = ['pythagorean theorem',
                'a squared plus b squared equals c squared.']
 
@@ -34,8 +45,26 @@ math_talk_2 = ['law of cosines',
 
 math_talk_3 = ['run a function', runFunction()]
 
+math_talk_4 = ['all sum and difference identities',
+               'sum and difference identities',
+               'every sum and difference identity',
+               'sin(a +/- b) = sin(a) * cos(b) +/- cos(a) * sin(b); cos(a +/- b) = cos(a) * sin(b) -/+ sin(a) * sin(b); tan(a +/- b) = (tan(a) +/- tan(b))/(1 -/+ tan(a) * tan(b))'
+               'sin sum and difference identity',
+               'sum and difference identity sin',
+               'sin itentity sum and difference'
+               'sin(a +/- b) = sin(a) * cos(b) +/- cos(a) * sin(b)',
+               'cos sum and difference identity',
+               'sum and difference identity cos',
+               'cos itentity sum and difference'
+               'cos(a +/- b) = cos(a) * sin(b) -/+ sin(a) * sin(b)',
+               'tan sum and difference identity',
+               'sum and difference identity tan',
+               'tan itentity sum and difference'
+               'tan(a +/- b) = (tan(a) +/- tan(b))/(1 -/+ tan(a) * tan(b))']
+               
+
 list_trainer = ListTrainer(my_bot)
-for item in (small_talk, math_talk_1, math_talk_2, math_talk_3):
+for item in (small_talk, math_talk_1, math_talk_2, math_talk_3, math_talk_4, farewell_talk):
     list_trainer.train(item)
 
 print(my_bot.get_response("3 plus 5"))
