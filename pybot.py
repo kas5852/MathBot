fc93c37c4fb6b08901546896d8159bcccfa4dc9d
Karim Sabar <karim.sabar@utexas.edu>
2019-11-21 19:07:19 -0600

    Difference and product work, need to uncomment a lot of stuff:

--- pybot.py
+++ pybot.py
@@ -1,39 +1,52 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
+import string
 
 def problemSolver():
     inputString = input("What kind of word problem would you like me to solve? You can say 'Quadratic' or 'algebra'. ")
-    if inputString == "algebra":
-        algebraSolver(inputString)
+    if not inputString:
+        algebraSolver()
     else: 
-        quadraticSolver(inputString)
+        quadraticSolver()
         
 
-def algebraSolver(inputString):
+def algebraSolver():
+    # inputString = input("What's your algebra problem? ")
+    inputString = "John has 3 apples and Jake has 4 apples. What is the difference of their apples?"
     digitFlag = False
     variable = {}
     listOfDigits = []
     listOfDigitsIndex = -1
+    
     for word in inputString.split():
+        word = "".join((char for char in word if char not in string.punctuation))
         if word.isdigit():
             digitFlag = True
             listOfDigits.append(int(word))
             listOfDigitsIndex+=1
         elif digitFlag:
             digitFlag = False
-            if variable[word] is None:
+            if word not in variable:
                 variable[word] = []
                 variable[word].append(listOfDigits[listOfDigitsIndex])
             else:
                 variable[word].append(listOfDigits[listOfDigitsIndex])
-    if product in inputString:
+    if 'product' in inputString:
         for key in variable:
+            print(key)
             productList = variable[key]
+            print(productList)
             result = 1 
             for x in productList:
                 result = result * x 
-            print("The product of all the " + key + " is " + result + '\n')
+            print("The product of all the " + str(key) + " is " + str(result) + '\n')
+    
+    elif 'difference' in inputString:
+        for key in variable:
+            subtractList = variable[key]
+            result = abs(subtractList[0]-subtractList[1])
+            print("The difference of the " + str(key) + " is " + str(result) + '\n')
 
         
 
@@ -99,30 +112,32 @@ def trainBot():
     math_talk_6 = ['Tan Identity'
                 'tan(a +/- b) = (tan(a) +/- tan(b))/(1 -/+ tan(a) * tan(b))']
 
-    math_talk_function = ['Solve a problem for me!', problemSolver()]
-    math_talk_function2 = ["I'd like you to solve a problem for me", problemSolver()]
-    math_talk_function3 = ["Can you solve a word problem?", problemSolver()]
+    # math_talk_function = ['Solve a problem for me!', problemSolver()]
+    # math_talk_function2 = ["I'd like you to solve a problem for me", problemSolver()]
+    # math_talk_function3 = ["Can you solve a word problem?", problemSolver()]
                 
-    talks.extend((small_talk, math_talk_1, math_talk_2, math_talk_3, 
-        math_talk_4, math_talk_5, math_talk_6, math_talk_10, math_talk_11, 
-        math_talk_function, math_talk_function2, math_talk_function3,
-        farewell_talk))
+    # talks.extend((small_talk, math_talk_1, math_talk_2, math_talk_3, 
+    #     math_talk_4, math_talk_5, math_talk_6, math_talk_10, math_talk_11, 
+    #     farewell_talk))
     
-    corpus_trainer = ChatterBotCorpusTrainer(my_bot)
-    corpus_trainer.train('chatterbot.corpus.english')
+    # corpus_trainer = ChatterBotCorpusTrainer(my_bot)
+    # corpus_trainer.train('chatterbot.corpus.english')
     
-    list_trainer = ListTrainer(my_bot)
-    for item in talks:
-        list_trainer.train(item)
+    # list_trainer = ListTrainer(my_bot)
+    # for item in talks:
+    #     list_trainer.train(item)
     return my_bot 
 
 
 if __name__ == "__main__":
     my_bot = trainBot()
-    inputString = input("Hi I'm MathBot! Ask me a math question. I can do basic arithemtic and give you some cool theorems and laws!")
+    inputString = input("Hi I'm MathBot! Ask me a math question. I can do basic arithemtic and solve word problems if you include 'word problem' in your query!")
     while inputString != "exit":
-        print(my_bot.get_response(inputString))
-        inputString = input("What else would you like me to do?")
+        # if "word problem" in inputString:
+        problemSolver()
+        # else:
+        #     print(my_bot.get_response(inputString))
+        #     inputString = input("What else would you like me to do?")
 
     print("I had a great time! Run me again for some more dope math.")
         
